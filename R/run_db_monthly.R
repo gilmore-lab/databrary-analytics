d7840e642d125ae0aa7e6d431bf173c25f294208
rogilmore <thatrickgilmore@gmail.com>
2019-05-01 09:59:21 -0400

    Add new databrary_citations_rpt

---
+++ R/run_db_monthly.R
@@ -0,0 +1,13 @@
+# Authenticate to Google for Sheets access
+run_db_monthly <- function(db_account = "rogilmore@psu.edu") {
+  db <- googlesheets::gs_title('Databrary-analytics')
+  rmarkdown::render('databrary_weekly_rpt.Rmd', 
+                    params = list(db_account = db_account,
+                                  update_gs = TRUE,
+                                  update_stats = TRUE)) 
+  rmarkdown::render('databrary_citations_rpt.Rmd', 
+                    params = list(db_account = db_account,
+                                  update_gs = TRUE,
+                                  update_stats = TRUE)) 
+  
+}
