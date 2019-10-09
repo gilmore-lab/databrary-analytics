052b8783305c1e1d6329f3337470212a5644193a
Andrea R Seisler <ars17@lxclusterapps.tlt.psu.edu>
2019-10-09 13:38:13 -0400

    update file copy function

--- weekly/copy_move_file.R
+++ weekly/copy_move_file.R
@@ -13,10 +13,9 @@ current_weekly_rpt <- list.files(path = "weekly",".html$")
 
 new_weekly_rpt <- paste0(Sys.Date(), '_', current_weekly_rpt)
 
-file.exists(paste0("/weekly/", new_weekly_rpt))
-file.copy(current_weekly_rpt, new_weekly_rpt)
+file.exists(paste0("weekly/", current_weekly_rpt))
+file.copy(paste0("weekly/",current_weekly_rpt), paste0("weekly/",new_weekly_rpt))
 
 library(filesstrings)
-file.move(new_weekly_rpt, paste0(getwd(),"old_weekly_reports", new_weekly_rpt))
-          
+file.move(paste0("weekly/",new_weekly_rpt), paste0("old_weekly_reports/", new_weekly_rpt))
           }
