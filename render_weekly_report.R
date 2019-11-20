e484e72001ab58256e98b182dde53da76c537e7a
Andrea R Seisler <ars17@lxclusterapps.tlt.psu.edu>
2019-11-20 08:44:28 -0500

    weekly report update

--- render_weekly_report.R
+++ render_weekly_report.R
@@ -1,7 +1,9 @@
 render_weekly_report <- function (db_account="yourname@email.com")
 {
   rmarkdown::render("weekly/databrary_weekly_report.Rmd", output_format = "html_document", 
-                    params = list(db_account = db_account))
+                    params = list(db_account = db_account, 
+                                  update_gs = TRUE,
+                                  update_stats = TRUE))
   source("weekly/copy_move_rpt.R")
   copy_move_rpt()
 }
