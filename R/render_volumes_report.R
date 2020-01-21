a969ac4fdf27fa35273b09680455b8c520626152
rogilmore <thatrickgilmore@gmail.com>
2020-01-21 13:19:08 -0500

    Rerender CSV to update; fix bug with shared_type variable.

--- R/render_volumes_report.R
+++ R/render_volumes_report.R
@@ -1,7 +1,7 @@
 # render shared-volumes-sessions.Rmd
 
-render_volumes_report <- function(db_account) {
+render_volumes_report <- function(db_account, use_saved_file = TRUE) {
   if (!is.character(db_account)) stop("Databrary login required.")
   rmarkdown::render("shared-volumes-sessions/shared-volumes-sessions.Rmd",
-                    params = list(db_login=db_account))  
+                    params = list(db_login=db_account, use_saved_file=use_saved_file))  
 }
