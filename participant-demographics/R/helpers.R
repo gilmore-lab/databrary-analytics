e9ef05d51b7adc0e82d7fbe2cdd3e26a850b9988
Rick Gilmore <rog1@psu.edu>
2021-08-31 12:25:14 -0400

    Run weekly

--- participant-demographics/R/helpers.R
+++ participant-demographics/R/helpers.R
@@ -1,9 +1,10 @@
 # helper functions for participant-demog-report
 
 # Temporary until we update the databraryapi package
-download_session_csv <- function(vol_id = 1, to_df = TRUE,
-                                 return_response = FALSE, vb = FALSE) {
-  
+download_session_csv <- function(vol_id = 1,
+                                 to_df = TRUE,
+                                 return_response = FALSE,
+                                 vb = FALSE) {
   # Error handling
   if (length(vol_id) > 1) {
     stop("vol_id must have length 1.")
@@ -12,32 +13,43 @@ download_session_csv <- function(vol_id = 1, to_df = TRUE,
     stop("vol_id must be an integer > 0.")
   }
   
-  if (vb) message(paste0("Downloading spreadsheet from volume ", vol_id))
+  if (vb)
+    message(paste0("Downloading spreadsheet from volume ", vol_id))
   
-  r <- httr::content(httr::GET(paste0("https://nyu.databrary.org/volume/",
-                                      vol_id, "/csv")), 'text', encoding='UTF-8')
+  r <-
+    httr::content(httr::GET(
+      paste0("https://nyu.databrary.org/volume/",
+             vol_id, "/csv")
+    ), 'text', encoding = 'UTF-8')
   
   if (is.null(r) | !stringr::str_detect(r, "session-id")) {
-    if (vb) message(paste0("No CSV data returned from volume ", vol_id))
+    if (vb)
+      message(paste0("No CSV data returned from volume ", vol_id))
     NULL
   } else if (to_df == TRUE) {
-    if (vb) message(paste0("Converting response to data frame."))
+    if (vb)
+      message(paste0("Converting response to data frame."))
     r_df <- read.csv(text = r, stringsAsFactors = FALSE)
-    if (class(r_df)=="data.frame") {
-      if (vb) message(paste0("Imported data frame. Cleaning up."))
+    if (class(r_df) == "data.frame") {
+      if (vb)
+        message(paste0("Imported data frame. Cleaning up."))
       r_df <- dplyr::mutate(r_df, vol_id = vol_id)
-      r_df <- dplyr::rename(r_df,
-                            session_id = session.id,
-                            session_name = session.name,
-                            session_date = session.date,
-                            session_release = session.release)
+      r_df <- dplyr::rename(
+        r_df,
+        session_id = session.id,
+        session_name = session.name,
+        session_date = session.date,
+        session_release = session.release
+      )
       r_df
     } else {
-      if (vb) message("Can't coerce to data frame. Skipping.\n")
+      if (vb)
+        message("Can't coerce to data frame. Skipping.\n")
       NULL
     }
   } else {
-    if (vb) message(paste0("Returning raw data from volume ", vol_id))
+    if (vb)
+      message(paste0("Returning raw data from volume ", vol_id))
     r
   }
 }
@@ -47,8 +59,15 @@ get_volume_demog <- function(vol_id) {
   v_ss <- download_session_csv(vol_id)
   
   if ("participant.birthdate" %in% names(v_ss)) {
-    dplyr::select(v_ss, vol_id, participant.birthdate, participant.race, 
-                  participant.ethnicity, participant.gender, participant.birthdate)
+    dplyr::select(
+      v_ss,
+      vol_id,
+      participant.birthdate,
+      participant.race,
+      participant.ethnicity,
+      participant.gender,
+      participant.birthdate
+    )
   } else {
     NULL
   }
@@ -56,12 +75,19 @@ get_volume_demog <- function(vol_id) {
 
 get_volume_birthdate <- function(vol_id) {
   v_ss <- download_session_csv(vol_id)
-  message(paste0("....Gathering participant.birthdate data from volume ", vol_id))
+  message(paste0(
+    "....Gathering participant.birthdate data from volume ",
+    vol_id
+  ))
   
   if ("participant.birthdate" %in% names(v_ss)) {
     dplyr::select(v_ss, vol_id, session_id, participant.birthdate)
   } else {
-    data.frame(vol_id = vol_id, session_id = NA,  participant.birthdate = NA)
+    data.frame(
+      vol_id = vol_id,
+      session_id = NA,
+      participant.birthdate = NA
+    )
   }
 }
 
@@ -69,39 +95,61 @@ get_volume_gender <- function(vol_id) {
   v_ss <- download_session_csv(vol_id)
   message(paste0("....Gathering participant.gender data from volume ", vol_id))
   
-    if ("participant.gender" %in% names(v_ss)) {
+  if ("participant.gender" %in% names(v_ss)) {
     dplyr::select(v_ss, vol_id, session_id, participant.gender)
   } else {
-    data.frame(vol_id = vol_id, session_id = NA,  participant.gender = NA)
+    data.frame(
+      vol_id = vol_id,
+      session_id = NA,
+      participant.gender = NA
+    )
   }
 }
 
 get_volume_race <- function(vol_id) {
   v_ss <- download_session_csv(vol_id)
   message(paste0("....Gathering participant.race data from volume ", vol_id))
-
-    if ("participant.race" %in% names(v_ss)) {
+  
+  if ("participant.race" %in% names(v_ss)) {
     dplyr::select(v_ss, vol_id, session_id, participant.race)
   } else {
-    data.frame(vol_id = vol_id, session_id = NA, participant.race = NA)
+    data.frame(
+      vol_id = vol_id,
+      session_id = NA,
+      participant.race = NA
+    )
   }
 }
 
 get_volume_ethnicity <- function(vol_id) {
   v_ss <- download_session_csv(vol_id)
-  message(paste0("....Gathering participant.ethnicity data from volume ", vol_id))
+  message(paste0(
+    "....Gathering participant.ethnicity data from volume ",
+    vol_id
+  ))
   
-    if ("participant.ethnicity" %in% names(v_ss)) {
+  if ("participant.ethnicity" %in% names(v_ss)) {
     dplyr::select(v_ss, vol_id, session_id, participant.ethnicity)
   } else {
-    data.frame(vol_id = vol_id, session_id = NA, participant.ethnicity = NA)
+    data.frame(
+      vol_id = vol_id,
+      session_id = NA,
+      participant.ethnicity = NA
+    )
   }
 }
 
-get_volumes_demo <- function(min_vol_id = 1, max_vol_id = 10) {
+get_volumes_demo <- function(min_vol_id = 1,
+                             max_vol_id = 10) {
   vols_range <- min_vol_id:max_vol_id
   
-  message(paste0("Getting demographic data for volumes ", min_vol_id, "-", max_vol_id, "\n"))
+  message(paste0(
+    "Getting demographic data for volumes ",
+    min_vol_id,
+    "-",
+    max_vol_id,
+    "\n"
+  ))
   
   message("...Gathering birthdates")
   bdts <- purrr::map_dfr(vols_range, get_volume_birthdate)
@@ -121,16 +169,30 @@ get_volumes_demo <- function(min_vol_id = 1, max_vol_id = 10) {
   m
 }
 
-save_volumes_demo <- function(df, min_vol_id, max_vol_id, 
+save_volumes_demo <- function(df, min_vol_id, max_vol_id,
                               dir = "participant-demographics/csv") {
-  message(paste0("Saving demographic data for volumes ", min_vol_id, "-", max_vol_id, "\n"))
-  fn <- paste0(dir, "/", stringr::str_pad(min_vol_id, 4, pad = "0"), "-", 
-               stringr::str_pad(max_vol_id, 4, pad = "0"), "-demog.csv")
+  message(paste0(
+    "Saving demographic data for volumes ",
+    min_vol_id,
+    "-",
+    max_vol_id,
+    "\n"
+  ))
+  fn <-
+    paste0(
+      dir,
+      "/",
+      stringr::str_pad(min_vol_id, 4, pad = "0"),
+      "-",
+      stringr::str_pad(max_vol_id, 4, pad = "0"),
+      "-demog.csv"
+    )
   fn
   readr::write_csv(df, fn)
 }
 
-get_save_volumes_demo <- function(min_vol_id = 1, max_vol_id = 10, 
+get_save_volumes_demo <- function(min_vol_id = 1,
+                                  max_vol_id = 10,
                                   dir = "participant-demographics/csv") {
   df <- get_volumes_demo(min_vol_id, max_vol_id)
   save_volumes_demo(df, min_vol_id, max_vol_id, dir)
@@ -138,10 +200,10 @@ get_save_volumes_demo <- function(min_vol_id = 1, max_vol_id = 10,
 
 
 # Downloads new demographic data from Databrary and saves it as a new CSV
-# 
+#
 # For efficiency reasons, the code takes 10 volumes at a time and saves them
 # in individual CSVs labeled with the volume numbers.
-regenerate_vol_demo_csvs <- function(new_vol_rg_min = 1261, 
+regenerate_vol_demo_csvs <- function(new_vol_rg_min = 1261,
                                      new_vol_rg_max = 1270,
                                      csv_dir = "participant-demographics/csv") {
   require(purrr)
@@ -153,26 +215,29 @@ regenerate_vol_demo_csvs <- function(new_vol_rg_min = 1261,
     stop('`new_vol_rg_min` must > 0.')
   }
   if (!is.numeric(new_vol_rg_max)) {
-    stop('`new_vol_rg_max` must be a number.')    
+    stop('`new_vol_rg_max` must be a number.')
   }
   if (new_vol_rg_max < 1) {
     stop('`new_vol_rg_max` must > 0.')
   }
   if (new_vol_rg_min > new_vol_rg_max) {
-    stop('`new_vol_rg_min` must < `new_vol_rg_max.')    
+    stop('`new_vol_rg_min` must < `new_vol_rg_max.')
   }
   if (!is_character(csv_dir)) {
-    stop('`csv_dir` must be a character string.')     
+    stop('`csv_dir` must be a character string.')
   }
   if (!dir.exists(csv_dir)) {
-    stop('Directory not found: `', csv_dir, '`.')       
+    stop('Directory not found: `', csv_dir, '`.')
   }
   
   lo <- seq(from = new_vol_rg_min, to = new_vol_rg_max, by = 10)
   hi <- seq(from = new_vol_rg_min + 9, to = new_vol_rg_max, by = 10)
   
   # Rick really loves functional programming
-  purrr::map2(.x = lo, .y = hi, get_save_volumes_demo, csv_dir)
+  purrr::map2(.x = lo,
+              .y = hi,
+              get_save_volumes_demo,
+              csv_dir)
 }
 
 load_demog_csvs <- function(dir = "participant-demographics/csv") {
@@ -190,7 +255,8 @@ load_demog_csvs <- function(dir = "participant-demographics/csv") {
   purrr::map_dfr(fl, readr::read_csv)
 }
 
-get_volumes_owners <- function(min_vol_id = 1, max_vol_id = 10) {
+get_volumes_owners <- function(min_vol_id = 1,
+                               max_vol_id = 10) {
   vols_range <- min_vol_id:max_vol_id
   message(".Gathering volume owners")
   purrr::map_dfr(.x = vols_range, .f = databraryapi::list_volume_owners)
@@ -201,42 +267,67 @@ get_volume_first_owner <- function(vol_id) {
   if (purrr::is_empty(df)) {
     NULL
   } else {
-    df[1,]
+    df[1, ]
   }
 }
 
-get_volumes_first_owners <- function(min_vol_id = 1, max_vol_id = 10) {
-  vols_range <- min_vol_id:max_vol_id
-  message(".Gathering volume first owners")
-  purrr::map_dfr(.x = vols_range, .f = get_volume_first_owner)
-}
+get_volumes_first_owners <-
+  function(min_vol_id = 1,
+           max_vol_id = 10) {
+    vols_range <- min_vol_id:max_vol_id
+    message(".Gathering volume first owners")
+    purrr::map_dfr(.x = vols_range, .f = get_volume_first_owner)
+  }
 
-save_volumes_owners <- function(df, min_vol_id, max_vol_id, 
+save_volumes_owners <- function(df,
+                                min_vol_id,
+                                max_vol_id,
                                 dir = "participant-demographics/csv",
                                 fn_suffix = "-owners.csv") {
-  fn <- paste0(dir, "/", stringr::str_pad(min_vol_id, 4, pad = "0"), "-", 
-               stringr::str_pad(max_vol_id, 4, pad = "0"), fn_suffix)
+  fn <-
+    paste0(
+      dir,
+      "/",
+      stringr::str_pad(min_vol_id, 4, pad = "0"),
+      "-",
+      stringr::str_pad(max_vol_id, 4, pad = "0"),
+      fn_suffix
+    )
   message(paste0(".Saving volume owner data to ", fn, "."))
   readr::write_csv(df, fn)
 }
 
-get_save_volumes_owners <- function(min_vol_id = 1, max_vol_id = 10, 
-                                    dir = "participant-demographics/csv") {
-  message(paste0("Getting owner data for volumes ", min_vol_id, "-", max_vol_id))
-  df <- get_volumes_owners(min_vol_id, max_vol_id)
-  message(paste0("Saving owner data for volumes ", min_vol_id, "-", max_vol_id))
-  save_volumes_owners(df, min_vol_id, max_vol_id, dir, fn_suffix = "-owners.csv")
-}
+get_save_volumes_owners <-
+  function(min_vol_id = 1,
+           max_vol_id = 10,
+           dir = "participant-demographics/csv") {
+    message(paste0("Getting owner data for volumes ", min_vol_id, "-", max_vol_id))
+    df <- get_volumes_owners(min_vol_id, max_vol_id)
+    message(paste0("Saving owner data for volumes ", min_vol_id, "-", max_vol_id))
+    save_volumes_owners(df, min_vol_id, max_vol_id, dir, fn_suffix = "-owners.csv")
+  }
 
-get_save_volumes_first_owners <- function(min_vol_id = 1, max_vol_id = 10,
-                                          dir = "participant-demographics/csv") {
-  message(paste0("Getting first owner data for volumes ", min_vol_id, "-", max_vol_id))
-  df <- get_volumes_first_owners(min_vol_id, max_vol_id)
-  message(paste0("Saving first owner data for volumes ", min_vol_id, "-", max_vol_id))
-  save_volumes_owners(df, min_vol_id, max_vol_id, dir, fn_suffix = "-first-owners.csv")
-}
+get_save_volumes_first_owners <-
+  function(min_vol_id = 1,
+           max_vol_id = 10,
+           dir = "participant-demographics/csv") {
+    message(paste0(
+      "Getting first owner data for volumes ",
+      min_vol_id,
+      "-",
+      max_vol_id
+    ))
+    df <- get_volumes_first_owners(min_vol_id, max_vol_id)
+    message(paste0(
+      "Saving first owner data for volumes ",
+      min_vol_id,
+      "-",
+      max_vol_id
+    ))
+    save_volumes_owners(df, min_vol_id, max_vol_id, dir, fn_suffix = "-first-owners.csv")
+  }
 
-load_owner_csvs <- function(dir = "participant-demographics/csv", 
+load_owner_csvs <- function(dir = "participant-demographics/csv",
                             fn_suffix = "-owners") {
   if (!is.character(dir)) {
     stop("'dir' must be a string")
@@ -245,7 +336,8 @@ load_owner_csvs <- function(dir = "participant-demographics/csv",
     stop("'dir' does not exist.")
   }
   
-  fl <- list.files(dir, paste0(fn_suffix, "\\.csv$"), full.names = TRUE)
+  fl <-
+    list.files(dir, paste0(fn_suffix, "\\.csv$"), full.names = TRUE)
   if (is.null(fl)) {
     stop(paste0("No csv files in ", dir, "."))
   }
@@ -257,10 +349,12 @@ render_participant_demog_report <- function(db_login) {
     stop('`db_login` must be a character string.')
   }
   
-  rmarkdown::render("participant-demographics/participant-demog-report.Rmd",
-                    params = list(db_login = db_login))
+  rmarkdown::render(
+    "participant-demographics/participant-demog-report.Rmd",
+    params = list(db_login = db_login)
+  )
   databraryapi::logout_db()
-  if(file.exists("participant-demographics/.databrary.RData")) {
+  if (file.exists("participant-demographics/.databrary.RData")) {
     file.remove("participant-demographics/.databrary.RData")
   }
 }
