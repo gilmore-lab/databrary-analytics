a056d1af6f509e7a7b3e505b0b6f70e3ddcbdb74
rogilmore <rog1@psu.edu>
2023-11-06 15:32:07 -0500

    Clean-up old Databrary analytics site.

--- for-researchers/README.md
+++
@@ -1,4 +0,0 @@
-# For researchers
-
-This folder contains code for reports that an individual Authorized Investigator on Databrary might want to run for their own use.
-
--- old_reports/R/render_volumes_report.R
+++
@@ -1,7 +0,0 @@
-# render shared-volumes-sessions.Rmd
-
-render_volumes_report <- function(db_account, use_saved_file = TRUE) {
-  if (!is.character(db_account)) stop("Databrary login required.")
-  rmarkdown::render("shared-volumes-sessions/shared-volumes-sessions.Rmd",
-                    params = list(db_login=db_account, use_saved_file=use_saved_file))  
-}
